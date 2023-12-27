23f89719d494ee666530db77b3999cbf9d133584
b286951543 <892153580@qq.com>
2023-12-27 23:30:08 +0800

    初始化

---
+++ api/department_api.py
@@ -0,0 +1,51 @@
+import random
+
+from api.enterprise_wechat_api import EnterpriseWechat
+
+
+class Department(EnterpriseWechat):
+    """
+    部门管理
+    """
+
+    def create(self, fail_example_data=None):
+        """
+        部门创建
+        传入反例数据时,会自动覆盖正例数据
+        :param fail_example_data: 反例数据
+        :return: 创建结果
+        """
+        json = {
+            "name": f'test_{random.randint(100, 999999)}',
+            "parentid": 1
+        }
+        url = 'cgi-bin/department/create'
+        # json 会自动放入请求体
+        res = self.enterprise_wechat_send('post', url, json=json, fail_example_data=fail_example_data)
+        return res
+
+    def get_sub_depart(self, parent_id=1):
+        """
+        获取子部门ID列表
+        :return: 结果
+        """
+        params = {
+            "id": parent_id
+        }
+        url = 'cgi-bin/department/simplelist'
+        # params 会自动放入 url
+        res = self.enterprise_wechat_send('get', url, params=params)
+        return res
+
+    def delete(self, depart_id):
+        """
+        删除部门
+        :return: 结果
+        """
+        params = {
+            "id": depart_id
+        }
+        url = 'cgi-bin/department/delete'
+        # params 会自动放入 url
+        res = self.enterprise_wechat_send('get', url, params=params)
+        return res
---
+++ testcase/test_department.py
@@ -0,0 +1,60 @@
+import pytest
+import allure
+
+from api.department_api import Department
+from util.yaml_util import read_data_yaml
+
+
+# 设置分层, 1级目录
+@allure.epic('企业微信')
+# 设置分层, 2级目录
+@allure.feature('部门管理')
+class TestDepartment:
+    """
+    部门管理测试
+    """
+
+    def setup_class(self):
+        """
+        实例化部门接口
+        :return:
+        """
+        self.dept_manage = Department()
+
+    @allure.title('创建部门')
+    def test_create_depart(self):
+        """
+        创建部门
+        :return:
+        """
+        res = self.dept_manage.create()
+        assert res.json()['errcode'] == 0
+
+    @allure.title('创建部门 反例')
+    @pytest.mark.parametrize('case', read_data_yaml('department_data.yaml'))
+    def test_create_depart_fail(self, case):
+        """
+        反例: 创建部门
+        :param case: 测试案例
+        :return:
+        """
+        for key, value in case.items():
+            res = self.dept_manage.create(value)
+            assert res.json()['errcode'] == value['expect']
+
+    @allure.title('获取子部门ID列表')
+    def test_sub_depart(self):
+        res = self.dept_manage.get_sub_depart(parent_id=1)
+        res_json = res.json()
+        id_list = [item['id'] for item in res_json['department_id']]
+        self.__depart_id_list = id_list
+        assert len(id_list) != 0
+
+    @allure.title('删除部门')
+    def test_delete_depart(self):
+        self.test_sub_depart()
+        # 删除 1号 部门
+        self.__depart_id_list.remove(1)
+        for depart_id in self.__depart_id_list:
+            res = self.dept_manage.delete(depart_id)
+            assert res.json()['errcode'] == 0
